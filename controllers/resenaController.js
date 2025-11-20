881ab2de5fe90d84539ff31ebbebe91102fc83bc
DarielOwl <darieldesosa@gmail.com>
2025-11-20 09:59:27 -0300

    refactor: Se elimina codigo viejo

--- controllers/resenaController.js
+++
@@ -1,8 +0,0 @@
-const resenaService = require('../services/resenaService');
-const Proveedor = require('../models/Proveedor');
-
-exports.listarParaProveedor = async (req, res) => {
-  const proveedor = await Proveedor.findOne({ usuario: req.user._id });
-  const resenas = await resenaService.listarParaProveedor(proveedor._id);
-  res.render('proveedor/resenas/lista', { resenas });
-};
--- services/resenaService.js
+++
@@ -1,7 +0,0 @@
-const Resena = require('../models/Resena');
-
-async function listarParaProveedor(proveedorId) {
-    return await Resena.find({ proveedor: proveedorId }).populate('autor');
-}
-
-module.exports = { listarParaProveedor };
